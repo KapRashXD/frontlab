ca6a48b0788e4c598946426fd0b7227416dfd9ec
Драган Максим <148593473+KapRashXD@users.noreply.github.com>
2026-05-20 22:43:03 +0300

    Task 1: Page state

---
+++ lab-03-dynamic-rendering/src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <button @click="toggleLoading">Toggle loading</button>
+        <button @click="toggleError">Toggle error</button>
+        <button @click="clearItems">Clear items</button>
+        <button @click="addSampleItems">Add sample items</button>
+  </div>
+  <div v-if="isLoading">Завантаження…</div>
+        <div v-else-if="hasError">Помилка завантаження</div>
+        <div v-else-if="items.length === 0">Немає даних</div>
+        <ul v-else>
+          <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+        </ul>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{ 
+        isLoading: false,
+        hasError: false,
+        items: ['Елемент 1', 'Елемент 2', 'Елемент 3']
+      }
+    },
+    methods: {
+      toggleLoading(){
+        this.isLoading = !this.isLoading;
+        if(this.isLoading){
+          this.hasError = false;
+        }
+      },
+      toggleError(){
+        this.hasError = !this.hasError;
+        if(this.hasError){
+          this.isLoading = false;
+        }
+      },
+      clearItems(){
+        this.items = [];
+      },
+      addSampleItems(){
+        this.items = ['Елемент 1', 'Елемент 2', 'Елемент 3'];
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
