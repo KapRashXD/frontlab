a8fab1591320ecec0727cab6f80a3794a12bc5ba
Драган Максим <148593473+KapRashXD@users.noreply.github.com>
2026-05-21 22:31:46 +0300

    Task 5: persist tasks to localStorage

--- lab-5-pinia-state/src/App.vue
+++ lab-5-pinia-state/src/App.vue
@@ -11,13 +11,21 @@
   import TaskList from './components/TaskList.vue'
   import TaskForm from './components/TaskForm.vue'
   import TaskStats from './components/TaskStats.vue';
-  
+  import { useTaskStore } from './stores/taskStore'
+  import { onMounted } from 'vue'
+
   export default{
     name: 'App',
     components: {
       TaskStats,
       TaskList,
       TaskForm
+    },
+    setup(){
+      const taskStore = useTaskStore();
+      onMounted(() => {
+        taskStore.loadFromStorage();
+      })
     }
   }
 </script>
--- lab-5-pinia-state/src/stores/taskStore.js
+++ lab-5-pinia-state/src/stores/taskStore.js
@@ -2,27 +2,42 @@ import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('taskStore', {
   state: () => ({
-    tasks: [
-      { id: 1, title: 'Завдання 1', done: false },
-      { id: 2, title: 'Завдання 2', done: true },
-    ],
+    tasks: [],
     filter: 'all',
   }),
   actions: {
     addTask(title) {
       this.tasks.push({ id: Date.now(), title, done: false })
+      this.saveToStorage()
     },
     toggleTask(id) {
       const task = this.tasks.find((t) => t.id === id)
       if (task) {
         task.done = !task.done
+        this.saveToStorage()
       }
     },
     removeTask(id) {
       this.tasks = this.tasks.filter((t) => t.id !== id)
+      this.saveToStorage()
     },
     setFilter(filter) {
       this.filter = filter
+      this.saveToStorage()
+    },
+    loadFromStorage() {
+      const tasks = localStorage.getItem('tasks')
+      if (tasks) {
+        this.tasks = JSON.parse(tasks)
+      }
+      const filter = localStorage.getItem('filter')
+      if (filter) {
+        this.filter = filter
+      }
+    },
+    saveToStorage() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      localStorage.setItem('filter', this.filter)
     },
   },
   getters: {
