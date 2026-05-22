0e634e2671e3878e5634e2e5eed0d02419f452b7
Драган Максим <148593473+KapRashXD@users.noreply.github.com>
2026-05-22 23:17:53 +0300

    Task 5: 404 route + back navigation

--- lab-6-router-navigation/src/router/index.js
+++ lab-6-router-navigation/src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import CatalogView from '../views/CatalogView.vue'
 import ProductDetailsView from '../views/ProductDetailsView.vue'
+
 import ProfileView from '../views/ProfileView.vue'
 import ProfileInfoView from '../views/ProfileInfoView.vue'
 import ProfileSettingsView from '../views/ProfileSettingsView.vue'
+
+import NotFoundView from '../views/NotFoundView.vue'
 
 const routes = [
   {
@@ -36,6 +40,11 @@ const routes = [
       { path: 'info', component: ProfileInfoView },
       { path: 'settings', component: ProfileSettingsView },
     ],
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFoundView,
   },
 ]
 
---
+++ lab-6-router-navigation/src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>404 - Сторінку не знайдено</h2>
+        <router-link to="/">На головну</router-link>
+        <button @click="goBack">Назад</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NotFoundView',
+        methods: {
+            goBack() {
+                this.$router.go(-1);
+            }
+        }
+    }
+</script>
