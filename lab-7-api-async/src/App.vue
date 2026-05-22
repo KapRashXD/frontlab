67890dcf2c6337e35e6c522d3e6ca584dad5e4b1
Драган Максим <148593473+KapRashXD@users.noreply.github.com>
2026-05-22 23:41:58 +0300

    Task 1: project scaffold + UI states

---
+++ lab-7-api-async/src/App.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <PostsView />
+</template>
+
+<script setup>
+  import PostsView from './views/PostsView.vue';
+</script>
+
+<style scoped></style>
---
+++ lab-7-api-async/src/views/PostsView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="max-width: 700px; margin: 0 auto;">
+        <h2>Каталог постів</h2>
+        <span v-if="isLoading">Завантаження...</span>
+        <span v-else-if="error">Помилка: {{ error }}</span>
+        <span v-else-if="items.length === 0">Пости не знайдено.</span>
+        <span v-else>
+            
+        </span>
+    </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+
+    const items = ref([]);
+    const isLoading = ref(false);
+    const error = ref(null);
+</script>
